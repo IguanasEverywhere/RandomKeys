0bc5188479bc32f9df266f24a8084231897e995e
Scott Schwab <schwabpiano@gmail.com>
2022-02-03 13:24:32 -0600

    Add Group 1 and Group2 buttons and change style appropriately

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ var randomBlackKey = Math.floor(Math.random() * 5);
 var allRoots = ["A", "B", "C", "D", "E", "F", "G", "D-flat/C#", "E-flat/D#", "G-flat/F#", "A-flat/G#", "B-flat/A#"];
 var randomAnyKey = Math.floor(Math.random() * 12);
 
+var groupOne = ["C","G","D","A","E"];
+var randomGroupOneKey = Math.floor(Math.random() * 5);
+
+var groupTwo = ["F","B","G-flat","D-flat"];
+var randomGroupTwoKey = Math.floor(Math.random() * 4);
+
 document.getElementById("whiteButton").addEventListener("click", displayWhiteKey);
 
 function displayWhiteKey() {
@@ -21,11 +27,37 @@ function displayBlackKey() {
     randomBlackKey = Math.floor(Math.random() * 5);
 }
 
+
+
+
+
+
 document.getElementById("allRootsButton").addEventListener("click", displayAnyKey);
 
 function displayAnyKey() {
     document.getElementById("displayKey").innerHTML=allRoots[randomAnyKey];
     randomAnyKey = Math.floor(Math.random() * 12);
+}
+
+
+
+
+
+
+
+document.getElementById("grp1").addEventListener("click", displayGrp1);
+
+function displayGrp1() {
+    document.getElementById("displayKey").innerHTML=groupOne[randomGroupOneKey];
+    randomGroupOneKey = Math.floor(Math.random() * 5);
+}
+
+
+document.getElementById("grp2").addEventListener("click", displayGrp2);
+
+function displayGrp2() {
+    document.getElementById("displayKey").innerHTML=groupTwo[randomGroupTwoKey];
+    randomGroupTwoKey = Math.floor(Math.random() * 4);
 }
 
 
